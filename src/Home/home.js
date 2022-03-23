ffd0d26063c86d5deda9af1a414eadb2e577fefd
milone1 <erickmilan1002@gmail.com>
2022-03-23 12:51:26 -0500

    list pokemones

--- src/Home/home.js
+++ src/Home/home.js
@@ -1,11 +1,22 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Container } from "@mui/material";
 import  { getDataFromPokemon } from '../peticiones/index';
 
 const Home = () => {
     //! variable que guarda la lista de pokemones.
-    const[pokemons, setPokemos] = useState([]);
-    
+    const[pokemons, setPokemon] = useState([]);
+    //? funcion que ejecuta la url.
+    const fetchPokemonList = async()=> {
+        const listPokemones = await getDataFromPokemon();
+        console.log(listPokemones.results);
+        setPokemon(listPokemones.results);
+    };
+    //* declaracion de useEffect.
+    useEffect(() => { 
+        fetchPokemonList();
+        //! importante: poner el array vacio para que esta funcion no se ejecute por siempre.
+        //! ya que la funcion que este dentro de useEffect se ejecutara de forma infinita.
+    }, []);
     return(
         <Container>
             <h1>POKEDEX</h1>
